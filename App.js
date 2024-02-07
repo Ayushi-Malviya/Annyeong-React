c4acc1936ebbd9b61ab1e27b11e4381ec6676232
Ayushi <ayushimalviya279@gmail.com>
2024-02-07 11:13:52 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h6", 
+{ id: "heading"}, 
+"Hello Ayushi from React!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
